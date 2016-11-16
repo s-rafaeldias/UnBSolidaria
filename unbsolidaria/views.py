fc9b1d34f29b3386bbd50a58df5b7d865a9abc72
Rafael Dias <rafa197795@gmail.com>
2016-11-16 10:19:31 -0200

    Refatoração das views.py

--- unbsolidaria/views.py
+++ unbsolidaria/views.py
@@ -2,5 +2,86 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, "home.html", {})
+class IndexView(generic.ListView):
+    template_name = '../templates/index.html'
+    context_object_name = {}
+
+def contato(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email,
+                          ['rafaeltbt@gmail.com'])
+                form = ContactForm()
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+        return render_to_response("contato/contato.html", {'form': form,
+                                                           'mensagem': 'Email enviado com sucesso!'},
+                                  context_instance=RequestContext(request))
+    return render(request, "contato/contato.html", {'form': form})
+
+def faq(request):
+    faq_list = FAQ.objects.all()
+    paginator = Paginator(faq_list, 5)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        perguntas = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        perguntas = paginator.page(paginator.num_pages)
+
+    return render_to_response('faq/lista.html',{'perguntas':
+        perguntas}, context_instance=RequestContext(request))
+
+def noticias(request):
+    noticias_list = Noticia.objects.all()
+    paginator = Paginator(noticias_list, 5)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        noticias = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        noticias = paginator.page(paginator.num_pages)
+
+    return render_to_response('noticia/lista.html',{'noticias':
+    noticias}, context_instance=RequestContext(request))
+
+def noticia(request):
+    try:
+        noticia_id = int(request.GET.get('noticia', '1'))
+    except ValueError:
+        noticia_id = 0
+
+    noticia = get_object_or_404(Noticia, pk=noticia_id)
+    return render_to_response('noticia/noticia.html',{'noticia':noticia}, context_instance=RequestContext(request))
+
+def noticias(request):
+    noticias_list = Noticia.objects.all()
+    paginator = Paginator(noticias_list, 5)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        noticias = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        noticias = paginator.page(paginator.num_pages)
+
+    return render_to_response('noticia/lista.html',{'noticias':
+    noticias}, context_instance=RequestContext(request))
