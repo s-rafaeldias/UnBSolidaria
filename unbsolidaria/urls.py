3fb5fc7037c9e4259f55135ee4dbafd761002386
Afonso Dias <afonso-dias2011@hotmail.com>
2016-12-02 09:28:58 -0200

    login e logout, porém sem seus caminhos os perfil setados

--- unbsolidaria/urls.py
+++ unbsolidaria/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Urls para usuarios nao logados
@@ -8,6 +9,8 @@ urlpatterns = [
     url(r'^contato/$', views.contato, name="contato"),
     url(r'^faq/$', views.faq, name="faq"),
     url(r'^registrar/$', views.UserFromView.as_view(), name='registrar'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     # url(r'^login/$', views.login, name="login"),
     # url(r'^signup/$', views.signup, name="signup"),
 ]
