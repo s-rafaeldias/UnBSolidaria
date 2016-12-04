a5910d5a000a1d9a90274e9b2a93cf1499c26589
Afonso Dias <afonso-dias2011@hotmail.com>
2016-12-04 16:07:39 -0200

    expansão do formulario

--- unbsolidaria/forms.py
+++ unbsolidaria/forms.py
@@ -7,11 +7,11 @@ class ContactForm(forms.Form):
     INPUT_CLASS = 'form-control input-lg'
 
     from_email = forms.EmailField(required=True,
-        widget=forms.EmailInput(attrs={'class':INPUT_CLASS, 'placeholder': 'Email*'}))
+                                  widget=forms.EmailInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Email*'}))
     subject = forms.CharField(required=True,
-        widget=forms.TextInput(attrs={'class':INPUT_CLASS,'placeholder': 'Assunto*'}))
+                              widget=forms.TextInput(attrs={'class': INPUT_CLASS, 'placeholder': 'Assunto*'}))
     message = forms.CharField(required=True,
-        widget=forms.Textarea(attrs={'class':INPUT_CLASS,'placeholder': 'Mensagem*'}))
+                              widget=forms.Textarea(attrs={'class': INPUT_CLASS, 'placeholder': 'Mensagem*'}))
 
 
 class UserForm(forms.ModelForm):
@@ -19,4 +19,5 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'cpf', 'cnpj', 'telephone', 'type',
+                  'gender', 'description']
