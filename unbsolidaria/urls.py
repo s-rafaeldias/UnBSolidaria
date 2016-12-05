4e57a8410170f98464f4b8f0d6f62fad35a34b65
Afonso Dias <afonso-dias2011@hotmail.com>
2016-12-05 02:20:50 -0200

    Editar e deletar usuário, e botões de usuário na navbar

--- unbsolidaria/urls.py
+++ unbsolidaria/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     url(r'^deletarTrabalho/(?P<pk>\d+)/$', views.TrabalhoDelete.as_view(), name="deletar-trabalho"),
     url(r'^visualizarTrabalho/(?P<pk>\d+)/$', views.TrabalhoDetailView.as_view(), name='visualizar-trabalho'),
     url(r'^registrar/$', views.UserFormView.as_view(), name='registrar'),
+    url(r'^user/(?P<pk>\d+)/$', views.UserUpdate.as_view(template_name='user_form.html'), name='user-update'),
+    url(r'user/(?P<pk>\d+)/delete/$', views.UserDelete.as_view(success_url='../'), name='user-delete'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '../'}, name='logout'),
 ]
