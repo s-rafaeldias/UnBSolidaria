71ae31367dc44560e1d17a3426a44667f76d2921
Rafael Dias <rafa197795@gmail.com>
2016-12-06 23:23:15 -0200

    Fix commit

--- web/settings.py
+++ web/settings.py
@@ -29,17 +29,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.contenttypes',
-    'admin_tools',
-    'admin_tools.dashboard',
-    'admin_tools.menu',
-    'admin_tools.theming',
+    'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.admin',
     'unbsolidaria',
     'report_builder',
 
@@ -62,19 +57,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        #'APP_DIRS': True,
+        'APP_DIRS': True,
         'OPTIONS': {
-            'loaders': [
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                    'admin_tools.template_loaders.Loader',
-			],
             'context_processors': [
-                'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
                 'django.template.context_processors.request',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
