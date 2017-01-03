2b101e234da237a3d6cd63de8884ea78f3ca54d8
Eduardo <eduardoaraujocunhaa@gmail.com>
2017-01-03 19:40:14 -0200

    api

--- web/urls.py
+++ web/urls.py
@@ -31,5 +31,7 @@ urlpatterns = [
     url(r'^admin_tools/', include('admin_tools.urls')),
 
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
